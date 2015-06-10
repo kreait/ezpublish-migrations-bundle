1be3a67e36ac75ad5263f3eb628e4f75410e36c0
Jérôme Gamez <jerome@gamez.name>
2015-06-10 18:57:49 +0200

    Move files in src and tests directories

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -13,5 +13,5 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-$loader = require __DIR__.'/vendor/autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('Doctrine\DBAL\Migrations\Tests', __DIR__.'/../vendor/doctrine/migrations/tests');
